bcb2f9f5195b30492fa927ca5ea1650ed269ffd9
Brice Leroy <bbrriiccee@gmail.com>
2010-01-17 12:50:51 -0800

    Add paginator in my utils library

--- utils.py
+++ utils.py
@@ -1,6 +1,29 @@
 from django.core.paginator import Paginator, InvalidPage, EmptyPage
 
 def paginate(records, request, items=10):
+    """
+    make paginate easy... :
+    
+    from common.utils import paginate
+    objects = MyModel.objects.all()
+    # paginate all the objects 5 items per pages
+    objects_paginated = paginate(objects, request, 5)
+    
+    
+    objects_paginated can now be used in your template
+    - objects_paginated.has_next (boolean if has next record)
+    - objects_paginated.has_previous (boolean if has previous record)
+    - objects_paginated.object_list (return objects)
+    - objects_paginated.page (actual page)...
+
+    To manage pages in your template:
+    {% if objects.has_next %}
+        <a href="?page={{ objects.next_page_number }}">next</a>
+    {% endif %}
+    {% if objects.has_previous %}
+        <a href="?page={{ objects.previous_page_number }}">previous</a>
+    {% endif %}
+    """
     page = request.GET.get('page', '1')
     paginator = Paginator(records, items)
  
